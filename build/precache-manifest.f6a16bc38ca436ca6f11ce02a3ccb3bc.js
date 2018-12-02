79ab6bf049db859c7bfb5d7e53232297f6954f26
dontrusu <denishandball1997@gmail.com>
2018-12-03 00:08:26 +0200

    homework

---
+++ build/precache-manifest.f6a16bc38ca436ca6f11ce02a3ccb3bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6bf319c1aefb045235d8",
+    "url": "/static/js/main.6bf319c1.chunk.js"
+  },
+  {
+    "revision": "f4f3ceda2cd3badd5973",
+    "url": "/static/js/1.f4f3ceda.chunk.js"
+  },
+  {
+    "revision": "6bf319c1aefb045235d8",
+    "url": "/static/css/main.9853c289.chunk.css"
+  },
+  {
+    "revision": "bbde420178aa5714c6f84ff1382146ce",
+    "url": "/index.html"
+  }
+];
