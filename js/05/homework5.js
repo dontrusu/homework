8a0fae96a86f504a4daaad67d33c38c19cb33312
dontrusu <denishandball1997@gmail.com>
2018-12-11 13:19:38 +0200

    only part

---
+++ js/05/homework5.js
@@ -0,0 +1,20 @@
+// 3 person
+
+var a = {
+	name: "Ivan",
+	surname: "Ivanov",
+	married: "yes",
+	children: "yes",
+}
+var b = {
+	name: "Petr",
+	surname: "Petrov",
+	fathername: "Petrovich",
+	pets: "dog",
+	}
+var c = {
+	name: "Yana",
+	surname: "Yakovleva",
+	age: "20",
+	sex: "female",
+}
