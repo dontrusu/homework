9a0065934c5bfc1b274ee4e3aa6112007c0b9176
dontrusu <denishandball1997@gmail.com>
2018-12-17 22:15:59 +0200

    cell highlighting

--- js/07/homework7.js
+++ js/07/homework7.js
@@ -4,7 +4,7 @@ for(var i = 1; i<10; i++){
 	tr = document.createElement("tr");	
 	multiplication.appendChild(tr);
 	if(i % 2 == 0){
-		tr.style.background="#f6f6ee"
+		tr.style.backgroundColor="#f6f6ee"
 	}
 	for(var j = 1; j<10; j++){
 		var td = document.createElement("td");
@@ -13,6 +13,15 @@ for(var i = 1; i<10; i++){
 		td.style.border = "1px solid black"
 		td.style.width = "40px"
 		td.style.height = "40px"
+		td.onmouseover = function(event) {
+ 			event.target.style.background = "#ffcccc";
+ 			//this.parentElement.style.backgroundColor = "#ffcccc";
+		}
+		td.onmouseout = function(event) {
+	  		event.target.style.background = "";
+	  		//this.parentElement.style.backgroundColor = "";
+  	}
 
 	}
 }
+
