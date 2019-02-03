b75edc82c1e8aa1ceb49145de2040e4ac20d8dd0
dontrusu <denishandball1997@gmail.com>
2019-02-04 01:37:18 +0200

    homework

---
+++ React/react-basics/src/App.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import './App.css';
+
+function jsonPost(url, data){
+    return fetch(url, {
+        method: "POST",
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .catch(error => new Error('jsonPost failed'))
+}
+
+
+class Inputs extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {disabled: true}
+  }
+
+    handleClick = () => {
+      this.props.onSend(this.nick.value, this.message.value)
+      this.message.value = ""
+    }
+
+    handleChange = () => {
+      this.setState({disabled: !(this.nick.value && this.message.value)})
+    }
+
+    render(){
+      return (
+        <div>
+          <input type = "text" placeholder = "nick" ref = {c => this.nick = c} onChange = {this.handleChange}/>
+          <input type = "text" placeholder = "message" ref = {c => this.message = c} onChange = {this.handleChange}/>
+          <button onClick = {(this.handleClick)} disabled = {this.state.disabled}>Send</button>
+        </div>
+        )
+  }
+}
+
+let ChatMessage = (props) => {
+  return(
+    <div>
+      <b>{props.nick}: </b>
+       {props.message}
+    </div>
+    )
+}
+
+
+class Chat extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {msg: []}
+    this.showMessages = this.showMessages.bind(this);
+  }
+  showMessages() {
+    var data = jsonPost("http://students.a-level.com.ua:10012", {func: "getMessages", messageId: this.state.msg.nextMessageId})
+    {data.then((data) => this.setState({msg: data.data}))}
+  }
+  componentDidMount(){
+    setInterval(this.showMessages, 2000)
+  }
+
+  render() {
+    return(
+      <div className = "chat">
+        {this.state.msg.reverse().map((object, i) =>  <ChatMessage nick = {object.nick} message = {object.message} key = {i} />)}
+      </div>  
+        )
+  }
+}
+
+
+let App = (props) => {
+  return(
+    <div className = "app">
+      <Inputs onSend = {(n,m)=> jsonPost("http://students.a-level.com.ua:10012", {func: 'addMessage', nick: n , message: m })}/>
+      <Chat />
+    </div>
+  )
+}
+
+export default App;
