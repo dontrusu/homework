28d7e9a98066c2b9db080d3c42a17c0389a25dcb
dontrusu <denishandball1997@gmail.com>
2019-02-20 00:48:28 +0200

    project

--- React/chess/src/App.js
+++ React/chess/src/App.js
@@ -8,108 +8,85 @@ import { faChessPawn, faChessBishop, faChessKing, faChessKnight, faChessQueen, f
 library.add(faChessPawn, faChessBishop, faChessKing, faChessKnight, faChessQueen, faChessRook)
 
 
+class Figure extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    let color = {w: "white", b: "black"}
+    let figure = {
+      p: "chess-pawn",
+      b: "chess-bishop",
+      k: "chess-king",
+      n: "chess-knight",
+      q: "chess-queen",
+      r: "chess-rook"
+    }
+    return (
+      <FontAwesomeIcon className = "figure" icon={figure[this.props.name]} color = {color[this.props.color]} onClick = {this.props.onClick}/>
+    )
+  }
+}
 
 class Square extends React.Component {
+  constructor(props) {
+  super(props)
+  }
   render() {
     return (
       <button className = "square">
-        {this.props.value}
+        {this.props.children}
       </button>
     )
   }
 }
 
-class Pawn extends React.Component {
-  constructor(color){
-    super(color)
-    this.state = {color: {color}}
-  }
-  render() {
-    return(
-      <FontAwesomeIcon icon="chess-pawn" className = {this.state.color + "Pawn"} />
-    )
-  }
-}
-
-class Bishop extends React.Component {
-  constructor(color){
-    super(color)
-    this.state = {color: color}
-  }
-  render() {
-    return(
-      <FontAwesomeIcon icon="chess-bishop" className = {this.state.color + "Bishop"} />
-    )
-  }
-}
-
-class King extends React.Component {
-  constructor(color){
-    super(color)
-    this.state = {color: color}
-  }
-  render() {
-    return(
-      <FontAwesomeIcon icon="chess-king" className = {this.state.color + "King"} />
-    )
-  }
-}
-
-class Knight extends React.Component {
-  constructor(color){
-    super(color)
-    this.state = {color: color}
-  }
-  render() {
-    return(
-      <FontAwesomeIcon icon="chess-knight" className = {this.state.color + "Knight"} />
-    )
-  }
-}
-
-class Queen extends React.Component {
-  constructor(color){
-    super(color)
-    this.state = {color: color}
-  }
-  render() {
-    return(
-      <FontAwesomeIcon icon="chess-queen" className = {this.state.color + "Queen"} />
-    )
-  }
-}
-
-class Rook extends React.Component {
-  constructor(color){
-    super(color)
-    this.state = {color: color}
-  }
-  render() {
-    return(
-      <FontAwesomeIcon icon="chess-rook" className = {this.state.color + "Rook"} />
-    )
-  }
-}
+var board = [
+      ['wr','wn','wb','wq','wk','wb','wn','wr'],
+      ['wp','wp','wp','wp','wp','wp','wp','wp'],
+      [' ',' ',' ',' ',' ',' ',' ',' '],
+      [' ',' ',' ',' ',' ',' ',' ',' '],
+      [' ',' ',' ',' ',' ',' ',' ',' '],
+      [' ',' ',' ',' ',' ',' ',' ',' '],
+      ['bp','bp','bp','bp','bp','bp','bp','bp'],
+      ['br','bn','bb','bq','bk','bb','bn','br'] ]
 
 class Board extends React.Component {
-  constructor(){
-    super()
+  constructor(props){
+    super(props)
+    this.state = {board: board, currentX: null, currentY: null}
+
   }
-  renderSquare(i) {
-    return <Square value = {i} />
+
+  
+
+  renderSquare(color, name, x,y) {
+
+    this.handleClick = () => {
+    this.setState({currentX: {x}, currentY: {y}})
+    
+    }
+
+    return(
+      <Square>
+        <Figure color = {color} name = {name} onClick = {this.handleClick} x={x} y={y}/>
+      </Square>
+    )
   }
+
   render(){
-    let board = []
+      let filledBoard = []
     for(let i = 0; i < 8; i++){
       let boardLine = []
       for(let j = 0; j < 8; j++){
-        boardLine.push(this.renderSquare(i*8+j))
+        var str = this.state.board[i][j]
+        boardLine.push(this.renderSquare(str.charAt(0), str.charAt(1), j, i))
       }
-      board.push(<div className = "line">{boardLine}</div>)
-    }
+      filledBoard.push(<div className = "line">{boardLine}</div>)
+    }    console.log(this.state)
     return(
       <>
-        {board}
+        {filledBoard}
       </>
     )
   }
@@ -119,12 +96,6 @@ class Board extends React.Component {
 let App = (props) => {
   return(
   <div className = "game">
-    <Rook />
-    <Knight />
-    <Bishop />
-    <King />
-    <Queen />
-    <Pawn />
     <Board />
   </div>
   )
