6924a6b731541f860243089e1b7e832d54d1a82e
dontrusu <denishandball1997@gmail.com>
2019-02-22 18:09:40 +0200

    project

--- React/chess/src/App.js
+++ React/chess/src/App.js
@@ -33,8 +33,9 @@ class Square extends React.Component {
   super(props)
   }
   render() {
+    console.log(this.props)
     return (
-      <button className = "square">
+      <button className = "square" style = {{border: this.props.available === true ? "3px solid red" : ""}}>
         {this.props.children}
       </button>
     )
@@ -45,7 +46,7 @@ var board = [
       ['wr','wn','wb','wq','wk','wb','wn','wr'],
       ['wp','wp','wp','wp','wp','wp','wp','wp'],
       [' ',' ',' ',' ',' ',' ',' ',' '],
-      [' ',' ',' ',' ',' ',' ',' ',' '],
+      [' ',' ',' ',' ','br ',' ',' ',' '],
       [' ',' ',' ',' ',' ',' ',' ',' '],
       [' ',' ',' ',' ',' ',' ',' ',' '],
       ['bp','bp','bp','bp','bp','bp','bp','bp'],
@@ -54,21 +55,69 @@ var board = [
 class Board extends React.Component {
   constructor(props){
     super(props)
-    this.state = {board: board, currentX: null, currentY: null}
+    this.state = {board: board, currentX: 0, currentY: 0}
 
   }
 
   
+   isCellAvailable(board, figureX, figureY, x, y){
+    //фигура, которой ходим
+    const figure  = board[figureY][figureX]
+    //находится ли вражеская фигура в проверяемой позиции x,y
+    const isEnemyInCell = board[y][x][0] !== figure[0] 
+    if (board[y][x][0] === figure[0]) return false;
+    const figures = {
+        p(){           
+           //белые пешки ходят вниз
+           const deltaY = figure[0] === 'w' ? +1 : -1;
+           return (
+               (figureY + deltaY === y) && //всегда след. строка доски
+               (
+                   (figureX === x && board[figureY+deltaY][figureX] === " ") || 
+                   (isEnemyInCell && Math.abs(figureX - x) === 1 && board[y][x][0] !== " ") 
+               )
+           )
+        },
+        r(){
+          return(
+            y === figureY && x !== figureX || y !== figureY && x === figureX  
+          )
+        },
+        b(){
+          return(
+            (x-y) === (figureX - figureY) || (x+y) === (figureX + figureY) 
+          )
+        },
+        n(){
+          return(
+            ((y === figureY + 1 ) && (x === figureX + 2 )) ||
+            ((y === figureY - 1 ) && (x === figureX - 2 )) || 
+            ((y === figureY + 2 ) && (x === figureX + 1 )) ||
+            ((y === figureY - 2 ) && (x === figureX - 1 )) ||
+            ((y === figureY + 1 ) && (x === figureX - 2 )) ||
+            ((y === figureY - 1 ) && (x === figureX + 2 )) || 
+            ((y === figureY - 2 ) && (x === figureX + 1 )) ||
+            ((y === figureY + 2 ) && (x === figureX - 1 )) 
+          )
+        },
+        q(){
+          return(figures.r() || figures.b())
+        },
+    }
+    return figures[figure[1]]()
+  }
 
   renderSquare(color, name, x,y) {
 
     this.handleClick = () => {
-    this.setState({currentX: {x}, currentY: {y}})
-    
+      this.setState({currentX: x, currentY: y})
+      this.isCellAvailable(this.state.board, this.state.currentX, this.state.currentY, x, y)
     }
 
+    this.isCellAvailable(this.state.board, this.state.currentX, this.state.currentY, x, y)
+
     return(
-      <Square>
+      <Square available = {this.isCellAvailable(this.state.board, this.state.currentX, this.state.currentY, x, y)}>
         <Figure color = {color} name = {name} onClick = {this.handleClick} x={x} y={y}/>
       </Square>
     )
@@ -80,7 +129,7 @@ class Board extends React.Component {
       let boardLine = []
       for(let j = 0; j < 8; j++){
         var str = this.state.board[i][j]
-        boardLine.push(this.renderSquare(str.charAt(0), str.charAt(1), j, i))
+        boardLine.push(this.renderSquare(str[0], str[1], j, i))
       }
       filledBoard.push(<div className = "line">{boardLine}</div>)
     }    console.log(this.state)
